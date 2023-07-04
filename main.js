326de7df29043d4bf454eb21dfb2eb9c1fb1263d
Idontknowwhatnametogive <122078653+Idontknowwhatnametogive@users.noreply.github.com>
2023-07-04 13:10:25 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("My first console project");
+var first_name="Neil";
+var last_name="Phadnis";
+var name= first_name.concat(last_name);
+console.log(name);
+
+var a=12;
+var b=4747444747;
+var add = a+b;
+console.log(add);
+
+var sub = a-b;
+console.log(sub);
